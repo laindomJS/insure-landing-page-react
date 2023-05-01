19a4fc8f74c0ae6906171ae2e7001f6559d70016
laindomJS <adrijar21@gmail.com>
2023-05-01 13:12:40 -0400

    added font

--- src/components/navbar/NavbarContainer.jsx
+++ src/components/navbar/NavbarContainer.jsx
@@ -19,14 +19,14 @@ export const NavbarContainer = () => {
   return (
     <nav className='w-full lg:w-4/5 flex items-center justify-between mx-auto py-4 md:py-8 px-4 md:px-0 relative'>
       <img className='cursor-pointer' src={logo} alt='logo of the insure company' />
-      <ul ref={menu} className='absolute top-16 left-0 md:top-0 md:left-0 bg-very-dark-violet md:bg-transparent md:static w-full min-h-screen md:min-h-fit md:w-fit flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-8 list-none font-custom pt-12 md:pt-0 transition-transform duration-300 translate-y-full md:translate-y-0'>
+      <ul ref={menu} className='absolute top-16 left-0 md:top-0 md:left-0 bg-[#2A2B32] bg-nav-pattern-mobile bg-no-repeat bg-bottom md:bg-transparent md:static w-full min-h-screen md:min-h-fit md:w-fit flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-8 list-none font-karla pt-12 md:pt-0 transition-transform duration-300 translate-y-full md:translate-y-0'>
         <li className='option'>How we work</li>
         <li className='option'>Blog</li>
         <li className='option'>Account</li>
         <li className='btn'>View Plans</li>
       </ul>
       {
-        condition === true
+        condition
         ? (
           <BiX onClick={handleMenu} className='md:hidden w-8 h-8' />
         )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'custom': ['DM Serif Display', 'sans-serif']
+        'custom': ['DM Serif Display', 'sans-serif'],
+        'karla': ['Karla', 'sans-serif']
       },
       colors: {
         'dark-violet': 'hsl(256, 26%, 20%)',
@@ -15,6 +16,9 @@ export default {
         'very-dark-violet': 'hsl(270, 9%, 17%)',
         'dark-grayish-violet': 'hsl(273, 4%, 51%)',
         'very-light-gray': 'hsl(0, 0%, 98%)'
+      },
+      backgroundImage: {
+        'nav-pattern-mobile': 'url("/src/assets/images/bg-pattern-mobile-nav.svg")'
       }
     },
   },
